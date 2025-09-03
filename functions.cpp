37088d4c289ffa8b85beb9ede207f4f45673a2fe
r-ayala <rayala29@student.cccd.edu>
2025-09-03 14:21:51 -0700

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,11 @@
+// Put your name here
+
+#include "functions.h"
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Implement the functions from functions.h here
+
---
+++ main.cpp
@@ -0,0 +1,15 @@
+// Put your name here
+
+#include <iostream>
+#include <string>
+
+#include "functions.h"
+
+using namespace std;
+
+int main(){
+
+  // Implement your main program here using the functions declared in funtions.h
+  
+  return 0;
+}
