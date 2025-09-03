f26a0b431170a9ce50e242fed3b51018d4d124a4
r-ayala <rayala29@student.cccd.edu>
2025-09-03 15:04:41 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,28 @@ using namespace std;
 int main(){
 
   // Implement your main program here using the functions declared in funtions.h
+
+  int numOfInputs = 4;
+  int inputs[4];
+  int spaceMaker = 15;
+  
+  for (int i = 0; i < 4; ++i)
+    {
+      inputs[i] = getInput();
+    }
+
+  // Following code prints out message with info
+  int vehicleNum = 1;
+cout << "Vehicle Time (seconds) Speed (m/s) Speed (mph)" << endl;
+  for (int i = 0; i < 4; ++i)
+    {
+      cout << vehicleNum << string(14, ' ') << inputs[i] << string (10, ' ') <<
+              inputs[i].getSpeed() << string (10, ' ') << inputs[i].getSpeed().convertSpeed(); << endl;
+
+      ++vehicleNum;
+    }
+  
+      
   
   return 0;
 }
